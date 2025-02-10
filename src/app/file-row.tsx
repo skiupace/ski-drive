48ca0602c2d0b6bd3c8c26696c483f471a378934
skiupace <ahmedalhakemee200335@gmail.com>
2025-02-10 09:23:03 +0300

    fixed breadcrumbs

--- src/app/file-row.tsx
+++ src/app/file-row.tsx
@@ -3,22 +3,22 @@ import { FileIcon, Folder as FolderIcon } from "lucide-react";
 
 export function FileRow(props: { file: File }) {
   const { file } = props;
-  return <li key={file.id} className="px-6 py-4 border-b border-gray-700 hover:bg-gray-750">
-  <div className="grid grid-cols-12 gap-4 items-center">
-    <div className="col-span-6 flex items-center">
-      <a 
-        href={file.url} 
-        className="flex items-center text-gray-100 hover:text-blue-400"
-        target="_blank"
-      >
-        <FileIcon className="mr-3" size={20} />
-        {file.name}
-      </a>
+    return <li key={file.id} className="px-6 py-4 border-b border-gray-700 hover:bg-gray-750">
+    <div className="grid grid-cols-12 gap-4 items-center">
+      <div className="col-span-6 flex items-center">
+        <a 
+          href={file.url} 
+          className="flex items-center text-gray-100 hover:text-blue-400"
+          target="_blank"
+        >
+          <FileIcon className="mr-3" size={20} />
+          {file.name}
+        </a>
+      </div>
+      <div className="col-span-3 text-gray-400">{"file"}</div>
+      <div className="col-span-3 text-gray-400">{file.size}</div>
     </div>
-    <div className="col-span-3 text-gray-400">{"file"}</div>
-    <div className="col-span-3 text-gray-400">{file.size}</div>
-  </div>
-</li>;
+  </li>;
 };
 
 export function FolderRow(props: { folder: Folder, handleFolderClick: () => void }) {
