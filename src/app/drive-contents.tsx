e63f451537eca059a92407f5e3e3c1880544a772
skiupace <ahmedalhakemee200335@gmail.com>
2025-03-02 04:20:32 +0300

    allow all files to be uploaded

--- src/app/drive-contents.tsx
+++ src/app/drive-contents.tsx
@@ -72,7 +72,7 @@ export default function DriveContents(props: {
           </ul>
         </div>
         <UploadButton 
-          endpoint="imageUploader" 
+          endpoint="driveUploader" 
           onClientUploadComplete={() => {
             navigate.refresh();
           }}
